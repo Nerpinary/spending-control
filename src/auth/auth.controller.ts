1bfcca35295c31918b3822c3f860c83d3ab75f02
Nerpinary <nerpinary5693@gmail.com>
2022-09-06 13:55:34 +0300

    creating user and costs models controllers and services

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,43 @@
+import {Body, Controller, HttpStatus, Post, Res, UseGuards} from "@nestjs/common";
+import {Response} from "express";
+import {UsersService} from "../users/users.service";
+import {CreateUserDto} from "./dto/create-user.dto";
+import {RegistrationGuard} from "./guards/registration.guard";
+import {LoginUserDto} from "./dto/login-user.dto";
+import {LoginGuard} from "./guards/login.guard";
+import {AuthService} from "./auth.service";
+
+@Controller('auth')
+export class AuthController {
+    constructor(
+        private usersService: UsersService,
+        private authService: AuthService
+    ) {}
+
+    @UseGuards(LoginGuard)
+    @Post('login')
+    async loginUser(
+        @Body() loginUserDto: LoginUserDto,
+        @Res() res: Response,
+    ) {
+        const user = await this.usersService.login(loginUserDto);
+
+        const access = await this.authService.generateAccessToken(user)
+        const refresh = await this.authService.generateRefreshToken(user._id as string)
+
+        res.statusCode = HttpStatus.OK;
+        return res.send({...access, ...refresh, username: user.username})
+    }
+
+    @UseGuards(RegistrationGuard)
+    @Post('registration')
+    async registrationUser(
+        @Body() createUserDto: CreateUserDto,
+        @Res() res: Response,
+    ) {
+    await this.usersService.registration(createUserDto);
+
+    res.statusCode = HttpStatus.CREATED;
+    return res.send('user created')
+    }
+}
